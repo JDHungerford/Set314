6801f96b9c3dc6f0ae2fa7da18c144d177ef8ff9
JDHungerford <37678680+JDHungerford@users.noreply.github.com>
2018-04-09 19:19:41 -0500

    Add files via upload
    Unsorted no comments

--- src/UnsortedSet.java
+++ src/UnsortedSet.java
@@ -1,9 +1,9 @@
 /*  Student information for assignment:
  *
- *  On <MY|OUR> honor, <NAME1> and <NAME2), this programming assignment is <MY|OUR> own work
+ *  On <MY|OUR> honor, Miles Chandler and Jacob Hungerford), this programming assignment is <MY|OUR> own work
  *  and <I|WE> have not provided this code to any other student.
  *
- *  Number of slip days used:
+ *  Number of slip days used: 0
  *
  *  Student 1 (Student whose turnin account is being used)
  *  UTEID:
@@ -12,10 +12,10 @@
  *  Section number:
  *
  *  Student 2
- *  UTEID:
- *  email address:
- *  Grader name:
- *  Section number:
+ *  UTEID: jdh5468
+ *  email address: JHungerford1516@utexas.edu
+ *  Grader name: Anthony
+ *  Section number: 51350
  *
  */
 
@@ -23,15 +23,110 @@ import java.util.Iterator;
 import java.util.ArrayList;
 
 /**
- * A simple implementation of an ISet. 
- * Elements are not in any particular order.
- * Students are to implement methods that 
- * were not implemented in AbstractSet and override
- * methods that can be done more efficiently. 
- * An ArrayList must be used as the internal storage container.
+ * A simple implementation of an ISet. Elements are not in any particular order.
+ * Students are to implement methods that were not implemented in AbstractSet
+ * and override methods that can be done more efficiently. An ArrayList must be
+ * used as the internal storage container.
  *
  */
 public class UnsortedSet<E> extends AbstractSet<E> {
-    private ArrayList<E> myCon;
+	private ArrayList<E> myCon;
+	private int size;
 
+	public UnsortedSet() {
+		myCon = new ArrayList<E>();
+	}
+
+	public boolean add(E item) {
+		if (this.contains(item))
+			return false;
+		else {
+			myCon.add(item);
+			size++;
+			return true;
+		}
+	}
+
+	public boolean addAll(ISet<E> otherSet) {
+		boolean changed = false;
+		for (E item : otherSet) {
+			boolean added = add(item);
+			if (added)
+				changed = true;
+		}
+		return changed;
+	}
+
+	public void clear() {
+		size = 0;
+		Iterator<E> removeIt = this.iterator();
+		while (removeIt.hasNext()) {
+			removeIt.next();
+			removeIt.remove();
+		}
+	}
+
+	public boolean contains(E item) {
+		Iterator<E> it = this.iterator();
+		while (it.hasNext()) {
+			if (it.next().equals(item))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean containsAll(ISet<E> otherSet) {
+		for (E item : otherSet) {
+			boolean contains = contains(item);
+			if (!contains)
+				return false;
+		}
+		return true;
+	}
+
+	public ISet<E> difference(ISet<E> otherSet) {
+		ISet<E> result = new UnsortedSet<E>();
+		for (E item : myCon) {
+			if (!otherSet.contains(item))
+				result.add(item);
+		}
+		return result;
+	}
+	
+	public ISet<E> intersection(ISet<E> otherSet) {
+		ISet<E> result = new UnsortedSet<E>();
+		for (E item : myCon) {
+			if (otherSet.contains(item))
+				result.add(item);
+		}
+		return result;
+	}
+
+	public Iterator<E> iterator() {
+		return myCon.iterator();
+	}
+
+	public boolean remove(E item) {
+		Iterator<E> removeIt = this.iterator();
+		boolean removed = false;
+		while (removeIt.hasNext() && !removed) {
+			if (removeIt.next().equals(item))
+				removed = true;
+			size--;
+		}
+		return removed;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public ISet<E> union(ISet<E> otherSet) {
+		ISet<E> result = new UnsortedSet<E>();
+		for (E item : myCon)
+			result.add(item);
+		for (E item : otherSet)
+			result.add(item);
+		return null;
+	}
 }
